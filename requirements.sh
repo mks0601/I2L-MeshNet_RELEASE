dcfaaaa9721e122e76c458042bcd02cf5c1260b6
Gyeongsik Moon <mks0601@gmail.com>
2020-08-15 10:51:00 +0900

    Create requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+pip install opencv-python
+pip install torchgeometry
+pip intsall transforms3d
+pip install pycocotools
+pip install git+https://github.com/scottandrews/chumpy.git@fe51783e0364bf1e9b705541e7d77f894dd2b1ac
