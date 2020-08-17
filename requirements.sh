297e005d881dfa3c91b747d3b836626027829ecf
Gyeongsik Moon <mks0601@gmail.com>
2020-08-18 07:40:00 +0900

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -3,3 +3,5 @@ pip install torchgeometry
 pip intsall transforms3d
 pip install pycocotools
 pip install git+https://github.com/scottandrews/chumpy.git@fe51783e0364bf1e9b705541e7d77f894dd2b1ac
+pip install pyrender
+pip install trimesh
