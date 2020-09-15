a00d8d8aa8002c5323697ff6f0673241887a1a14
Gyeongsik Moon <mks0601@gmail.com>
2020-09-15 16:28:15 +0900

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,6 @@
 pip install opencv-python
 pip install torchgeometry
-pip intsall transforms3d
+pip install transforms3d
 pip install pycocotools
 pip install git+https://github.com/scottandrews/chumpy.git@fe51783e0364bf1e9b705541e7d77f894dd2b1ac
 pip install pyrender
